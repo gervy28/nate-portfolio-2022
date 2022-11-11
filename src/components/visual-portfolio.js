e84ed4abb63e0eefc49ba31f069a9242973e5419
Nate Gervenak <gervenaknate@gmail.com>
2022-11-11 06:20:49 -0600

    feature graphql query to portfolio

--- src/components/visual-portfolio.js
+++ src/components/visual-portfolio.js
@@ -9,37 +9,57 @@
  import * as visualPortfolioStyles from "./visual-portfolio.module.css"
  import JSONData from "../../content/resume/structured-resume.json"
  import { GatsbyImage, getImage } from "gatsby-plugin-image"
- import { StaticQuery, graphql } from "gatsby"
+ import { useStaticQuery, graphql } from "gatsby"
 
  
  const VisualPortfolio = () => {
-  const jobs = JSONData['work'];
-  // const data = useStaticQuery(graphql`
-  //   query ResumeQuery {
-  //     site {
-  //       siteMetadata {
-  //         resume {
-  //           technology_skills,
-  //           knowledge_domains,
-  //           tools,
-  //           soft_skills
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  // const jobs = JSONData['work'];
+  const data = useStaticQuery(graphql`
+    query Resume {
+      resumeJson {
+        work {
+          company
+          description
+          endDate
+          startDate
+          logo
+          position
+          name
+          location
+          summary
+          url
+          website
+          reference {
+            review
+            reviewer
+          }
+          highlights {
+            description
+            image
+            projectName
+            skills
+            tools
+          }
+        }
+      }
+    }
+  `)
+  
+  const testImage = data.resumeJson.work[0].highlights[0].image
+  const myImage = getImage(testImage);
+  
 
    return (
     <section className={visualPortfolioStyles.portfolio_section_container}>
       <h2 className="title_large">A FOUNDATION IN 'INTRA' AND 'ENTRE' - PRENEURSHIP</h2>
-        {jobs.map(entry => {
+        {data.resumeJson.work.map(entry => {
           return (
             <div>
               <h3 className="title_stretched">{entry["name"]}</h3>
               <div className={visualPortfolioStyles.portfolio_items_wrapper}>
                 {entry['highlights'].map(highlight => {
                   return (
-                    <div >
+                    <div className={visualPortfolioStyles.portfolio_item}>
                       <h4>{highlight.projectName}</h4>
                       <p>{highlight.description}</p>
                       <ul>
