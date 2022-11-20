ae840b6fc5632b599c850d3df3f7ec6c58d6fe13
Nate Gervenak <gervenaknate@gmail.com>
2022-11-19 18:31:12 -0600

    feature(slider): implemented horizontal scroll by drag
    Horizontal scroll feature with JS funcitons. A very messy commit in the
    visual-portfolio.js file, needs major refactoring to include:
    
    - Slider Converted Into Componenet

--- src/components/visual-portfolio.js
+++ src/components/visual-portfolio.js
@@ -5,8 +5,7 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
- import * as React from "react"
- import { useRef, createRef } from "react"
+ import React, { useRef, createRef, useState } from "react"
  import * as visualPortfolioStyles from "./visual-portfolio.module.css"
  import { GatsbyImage, getImage, getSrc } from "gatsby-plugin-image"
  import { useStaticQuery, graphql } from "gatsby"
@@ -48,6 +47,10 @@
     );
   }
 
+  const [ isScrolling, activateScrolling ] = useState(false);
+  const [ leftPosition, setLeftPosition ] = useState(0);
+  const [ startXPos, setStartXPos] = useState(); 
+
   const scrollableSections = useRef([]);  
 
   scrollableSections.current = data.resumeJson.work.map((element, i) => 
@@ -55,13 +58,37 @@
   );
 
   
-  function handleMouseDown(e) {
-    console.log(e);
+  function handleMouseDown(e, element) {
+    // mouses starting coordinate
+    activateScrolling(true);
+    // get the x position of the cursor
+    setStartXPos(e.pageX - element.current.offsetLeft);
+    // get the current elements distance scrolled left
+    setLeftPosition(element.current.scrollLeft);
   }
 
-  // const testImage = data.resumeJson.work[0].highlights[0]["projectName"]
-  // const myImage = getSrc(testImage);
-  
+  function handleMouseLeave() {
+    activateScrolling(false);
+  }
+
+  function handleMouseUp(e, element){
+    activateScrolling(false);
+  }
+
+  function handleMouseMove(e, element){
+    if(!isScrolling) return;
+
+
+    console.log(startXPos);
+
+    e.preventDefault();
+    const x = e.pageX - element.current.offsetLeft;
+    const walk = (x - startXPos);
+    console.log(walk);
+    element.current.scrollLeft = leftPosition - walk;
+    // console.log(walk);
+  }
+
 
 
    return (
@@ -74,7 +101,13 @@
             <h3 className="title_stretched">{entry["company"]}</h3>
 
             {/* Start Card Wrapper -- where scrolling occurs*/}
-            <div key={i} onMouseDown={() => handleMouseDown(scrollableSections.current[i])} ref={scrollableSections.current[i]} className={visualPortfolioStyles.portfolio_items_wrapper}>
+            <div key={i} 
+              onMouseDown={(e) => handleMouseDown(e, scrollableSections.current[i])} 
+              onMouseLeave={handleMouseLeave}
+              onMouseUp={handleMouseUp}
+              onMouseMove={(e) => handleMouseMove(e, scrollableSections.current[i])}
+              ref={scrollableSections.current[i]} 
+              className={visualPortfolioStyles.portfolio_items_wrapper}>
 
               {/* Render the individual card */}
               {entry['highlights'].map(highlight => {
